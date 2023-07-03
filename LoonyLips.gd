c0272f0c265e30c7227178555bfd4d7129f29cf8
Pahutich <dima.gamedev@pm.me>
2023-07-03 20:44:46 +0200

    2.4

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -4,5 +4,6 @@ extends Control
 func _ready():
 	var prompts = ["Dima", "Minions", "greatest"]
 	var story = "Once upon a time %s watched %s and thought it was the %s movie of the past two decades"
-	print(story % prompts)
+	story = story % prompts
 	
+	$DisplayText.text = story
