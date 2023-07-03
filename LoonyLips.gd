7bc516b9d642a7bd00f240c0b273c2093ac726b2
Pahutich <dima.gamedev@pm.me>
2023-07-03 16:30:45 +0200

    2.3

---
+++ LoonyLips.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+func _ready():
+	var prompts = ["Dima", "Minions", "greatest"]
+	var story = "Once upon a time %s watched %s and thought it was the %s movie of the past two decades"
+	print(story % prompts)
+	
