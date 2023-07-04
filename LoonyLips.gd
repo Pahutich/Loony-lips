ca745d33eefda8fde16926f4f12b0849cda79df1
Pahutich <dima.gamedev@pm.me>
2023-07-04 13:35:10 +0200

    2.9

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,19 +1,31 @@
 extends Control
 
 var player_words = []
-var prompts = ["a name", "a noun", "adverb", "adjective"]
-var story = "Once upon a time someone called %s ate a %s flavoured sandwich which made him feel all %s inside. It was a %s day "
+var template = [
+		{
+		"prompts" : ["a name", "a noun", "adverb", "adjective"],
+		"story" : "Once upon a time someone called %s ate a %s flavoured sandwich which made him feel all %s inside. It was a %s day "
+		},
+		{
+		"prompts" : ["a verb", "adjective", "a noun", "a verb"],
+		"story" : "I was doing %s and it felt %s. A little did I know that a %s was %s nearby"
+		}
+		]
+var current_story
 
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
 onready var DisplayText = $VBoxContainer/DisplayText
 onready var Label = $VBoxContainer/HBoxContainer/Label
 
 func _ready():
+	set_current_story()
 	DisplayText.text = "Welcome to Loony Lips! We are going to tell a story and have a wonderful time! "
 	check_player_words_length()
 	PlayerText.grab_focus()
 	
-#	$VBoxContainer/PlayerText.text = story
+func set_current_story():
+	randomize()
+	current_story = template[randi() % template.size()]
 
 func _on_PlayerText_text_entered(new_text):
 	add_to_player_words()
@@ -31,7 +43,7 @@ func add_to_player_words():
 	check_player_words_length()
 
 func is_story_done():
-	return player_words.size() == prompts.size()
+	return player_words.size() == current_story.prompts.size()
 	
 func check_player_words_length():
 	if is_story_done():
@@ -40,10 +52,10 @@ func check_player_words_length():
 		prompt_player()
 		
 func tell_story():
-	DisplayText.text = story % player_words
+	DisplayText.text = current_story.story % player_words
 	
 func prompt_player():
-	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
+	DisplayText.text += "May I have " + current_story.prompts[player_words.size()] + " please?"
 
 func end_game():
 	PlayerText.queue_free()
